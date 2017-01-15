6e7ed84316c9032cdea92abb3722cffa0c1f92cd
Bill <ZhangCheng@Bills-MacBook-Air.local>
2017-01-15 23:45:21 +0800

    新建工程

---
+++ CoreText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreText
+//
+//  Created by Bill on 1/15/17.
+//  Copyright © 2017 zmcyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreText/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  CoreText
+//
+//  Created by Bill on 1/15/17.
+//  Copyright © 2017 zmcyou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
